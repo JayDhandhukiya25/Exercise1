c276882b1aa486d4390840b83fe381334a14bd14
JayDhandhukiya25 <134092420+JayDhandhukiya25@users.noreply.github.com>
2023-06-02 00:05:30 -0400

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,11 +45,11 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is DHAVAL, but you can call me prof, DHAVAL or sir");
+        System.out.println("My name is JAY, but you can call me JD");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Be more active on essential skill");
         System.out.println("-- Have a semester with no violations of academic integrity!");
         System.out.println();
 
@@ -57,7 +57,7 @@ public class CardTrick {
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Swimming");
 
         System.out.println();
     }
